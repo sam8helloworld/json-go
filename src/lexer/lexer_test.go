82ddd7aad64161f486daa972030c46b94ba46658
sam8helloworld <samcreate0830@gmail.com>
2022-04-30 01:30:56 +0900

    test: 字句解析器のnull判定のテスト

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -159,6 +159,72 @@ func TestFailedBoolTokenize(t *testing.T) {
 	}
 }
 
+func TestSuccessNullTokenize(t *testing.T) {
+	f, err := os.Open("../testdata/null_only.json")
+	if err != nil {
+		fmt.Println("error")
+	}
+	defer f.Close()
+
+	// 一気に全部読み取り
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("error")
+	}
+	sut := NewLexer(string(b))
+	got, err := sut.Execute()
+	if err != nil {
+		t.Fatalf("failed to execute lexer %#v", err)
+	}
+	want := &[]token.Token{
+		{
+			Type:    token.LeftBraceType,
+			Literal: "{",
+		},
+		{
+			Type:    token.StringType,
+			Literal: "null",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NullType,
+			Literal: "null",
+		},
+		{
+			Type:    token.RightBraceType,
+			Literal: "}",
+		},
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Fatalf("got differs: (-got +want)\n%s", diff)
+	}
+}
+
+func TestFailedNullTokenize(t *testing.T) {
+	f, err := os.Open("../testdata/null_only_fragile.json")
+	if err != nil {
+		fmt.Println("error")
+	}
+	defer f.Close()
+
+	// 一気に全部読み取り
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("error")
+	}
+	sut := NewLexer(string(b))
+	got, err := sut.Execute()
+	if got != nil {
+		t.Errorf("want error %v, but got result %v", ErrNullTokenize, got)
+	}
+	if !errors.Is(err, ErrNullTokenize) {
+		t.Fatalf("want ErrNullTokenize, but got %v", err)
+	}
+}
+
 func TestFailedLexer(t *testing.T) {
 	f, err := os.Open("../testdata/lexer_structure_fragile.json")
 	if err != nil {
