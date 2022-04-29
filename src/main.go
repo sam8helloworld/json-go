584d9abdc0e6c54c473db267a6bef5d9e9cabb00
sam8helloworld <samcreate0830@gmail.com>
2022-04-29 23:31:42 +0900

    feat: 字句解析器の呼び出し

--- src/main.go
+++ src/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/sam8helloworld/json-go/lexer"
 )
 
 func main() {
 	fmt.Println("ファイル読み取り処理を開始します")
 	// ファイルをOpenする
-	f, err := os.Open("testdata/testdata01.json")
+	f, err := os.Open("testdata/string_only.json")
 	if err != nil {
 		fmt.Println("error")
 	}
@@ -20,6 +22,8 @@ func main() {
 	if err != nil {
 		fmt.Println("error")
 	}
+	lexer := lexer.NewLexer(string(b))
+	lexer.Execute()
 	// 出力
 	fmt.Println(string(b))
 }
