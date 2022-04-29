87b88461851e1794abc1ea3d06da5a8e6861c151
sam8helloworld <samcreate0830@gmail.com>
2022-04-29 19:47:52 +0900

    feat: ファイルを文字列として読み込む

--- src/main.go
+++ src/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 func main() {
-	println("hello world")
+	fmt.Println("ファイル読み取り処理を開始します")
+	// ファイルをOpenする
+	f, err := os.Open("testdata/testdata01.json")
+	if err != nil {
+		fmt.Println("error")
+	}
+	defer f.Close()
+
+	// 一気に全部読み取り
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("error")
+	}
+	// 出力
+	fmt.Println(string(b))
 }
