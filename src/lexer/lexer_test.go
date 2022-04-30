7d6cef3b73b06b71408fb09cd2a5f689a6e5f105
sam8helloworld <samcreate0830@gmail.com>
2022-04-30 18:21:53 +0900

    test: 字句解析器で数値を判別するテスト

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -225,6 +225,209 @@ func TestFailedNullTokenize(t *testing.T) {
 	}
 }
 
+func TestSuccessNumberTokenize(t *testing.T) {
+	f, err := os.Open("../testdata/number_only.json")
+	if err != nil {
+		fmt.Println("error")
+	}
+	defer f.Close()
+
+	// 一気に全部読み取り
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("error")
+	}
+	sut := NewLexer(string(b))
+	got, err := sut.Execute()
+	if err != nil {
+		t.Fatalf("failed to execute lexer %#v", err)
+	}
+	want := &[]token.Token{
+		{
+			Type:    token.LeftBraceType,
+			Literal: "{",
+		},
+		// "int": 100
+		{
+			Type:    token.StringType,
+			Literal: "int",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: "100",
+		},
+		{
+			Type:    token.CommaType,
+			Literal: ",",
+		},
+		// "float": 1.234
+		{
+			Type:    token.StringType,
+			Literal: "float",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: "1.234",
+		},
+		{
+			Type:    token.CommaType,
+			Literal: ",",
+		},
+		// "floatDotStart": .1234
+		{
+			Type:    token.StringType,
+			Literal: "floatDotStart",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: ".1234",
+		},
+		{
+			Type:    token.CommaType,
+			Literal: ",",
+		},
+		// "exponentialSmall": 1e10
+		{
+			Type:    token.StringType,
+			Literal: "exponentialSmall",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: "1e10",
+		},
+		{
+			Type:    token.CommaType,
+			Literal: ",",
+		},
+		// "exponentialLarge": 1E10
+		{
+			Type:    token.StringType,
+			Literal: "exponentialLarge",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: "1E10",
+		},
+		{
+			Type:    token.CommaType,
+			Literal: ",",
+		},
+		// "exponentialPlus": 1e+10
+		{
+			Type:    token.StringType,
+			Literal: "exponentialPlus",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: "1e+10",
+		},
+		{
+			Type:    token.CommaType,
+			Literal: ",",
+		},
+		// "exponentialMinus": 1e-10
+		{
+			Type:    token.StringType,
+			Literal: "exponentialMinus",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: "1e-10",
+		},
+		{
+			Type:    token.CommaType,
+			Literal: ",",
+		},
+		// "plus": +10
+		{
+			Type:    token.StringType,
+			Literal: "plus",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: "+10",
+		},
+		{
+			Type:    token.CommaType,
+			Literal: ",",
+		},
+		// "minus": -10
+		{
+			Type:    token.StringType,
+			Literal: "minus",
+		},
+		{
+			Type:    token.ColonType,
+			Literal: ":",
+		},
+		{
+			Type:    token.NumberType,
+			Literal: "-10",
+		},
+		{
+			Type:    token.RightBraceType,
+			Literal: "}",
+		},
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Fatalf("got differs: (-got +want)\n%s", diff)
+	}
+}
+
+func TestFailedNumberTokenize(t *testing.T) {
+	f, err := os.Open("../testdata/number_only_fragile.json")
+	if err != nil {
+		fmt.Println("error")
+	}
+	defer f.Close()
+
+	// 一気に全部読み取り
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("error")
+	}
+	sut := NewLexer(string(b))
+	got, err := sut.Execute()
+	if got != nil {
+		t.Errorf("want error %v, but got result %v", ErrNumberTokenize, got)
+	}
+	if !errors.Is(err, ErrNumberTokenize) {
+		t.Fatalf("want ErrNumberTokenize, but got %v", err)
+	}
+}
+
 func TestFailedLexer(t *testing.T) {
 	f, err := os.Open("../testdata/lexer_structure_fragile.json")
 	if err != nil {
